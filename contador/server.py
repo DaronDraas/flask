a235c7080f8bde7a28a8a4beda84ae0bf2576770
Esteban García <esteban.garcia@outlook.cl>
2022-07-16 15:05:41 -0400

    SubiendoContador

---
+++ contador/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+app.secret_key = "@polo57h"
+
+@app.route('/')
+def countbase():
+    if "visits" in session:
+        session['visits'] +=1
+    else:
+        print("key does not exist")
+        session['visits'] = 1
+    return render_template("index.html")
+
+@app.route('/count')
+def count():
+    session['visits'] += 1
+    return redirect('/')
+
+@app.route('/reset')
+def reseteo():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
